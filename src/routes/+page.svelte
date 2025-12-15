52016c5af8aaee1c261e751cf4f6fb3191774feb
haliacmon <tuna271@gmail.com>
2025-12-16 01:58:57 +0300

    basic drawer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+</script>
+
+<Drawer.Root>
+	<Drawer.Trigger class={buttonVariants({ variant: 'default', class: "m-2"  })}>Open</Drawer.Trigger>
+	<Drawer.Content>
+		<Drawer.Header>
+			<Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
+			<Drawer.Description>This action cannot be undone.</Drawer.Description>
+		</Drawer.Header>
+		<Drawer.Footer>
+			<Button>Submit</Button>
+			<Drawer.Close>Cancel</Drawer.Close>
+		</Drawer.Footer>
+	</Drawer.Content>
+</Drawer.Root>
